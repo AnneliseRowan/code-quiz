d373ddbd5cf5a100872c9c17319ce2a022883abb
Annélise Rowan <40217057+AnneliseRowan@users.noreply.github.com>
2021-04-18 10:56:38 -0400

    added styling and html, practicing to see how to change questions

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+let questions = [
+    {
+        question : "Who possesses the ring of power at the start of the trilogy?",
+        choiceA : "Gandalf",
+        choiceB : "Frodo",
+        choiceC : "Bilbo",
+        choiceD : "Sauron", 
+        correct : "Sauron"
+    },
+    {
+        question : "Who do the hobbits encounter at the Prancing Pony?",
+        choiceA : "Gandalf",
+        choiceB : "Legolas",
+        choiceC : "Boromir",
+        choiceD : "Strider",
+        correct : "Strider"
+    },
+    {
+        question : "How many members make up the fellowship of the ring?",
+        choiceA : "Seven",
+        choiceB : "Nine",
+        choiceC : "Eight",
+        choiceD : "Ten",
+        correct : "Seven"
+    },
+    {
+        question : "What is the name of the elf in the fellowship?",
+        choiceA : "Legolas",
+        choiceB : "Gimli",
+        choiceC : "Boromir",
+        choiceD : "Thranduil",
+        correct : "Legolas"
+    },
+    {
+        question : "What is the name of the dwarf in the fellowship?",
+        choiceA : "Merry",
+        choiceB : "Pippin",
+        choiceC : "Gimli",
+        choiceD : "Sauron",
+        correct : "Gimli"
+    }
+];
 
